52e193f1147b732f9b4c32c096918b5326bc3a44
Elkin Akhundzada <eakhundzade@gmail.com>
2019-07-02 23:54:50 +0400

    Migrations

---
+++ app/migrations/1.0.2/algo.php
@@ -0,0 +1,84 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class AlgoMigration_102
+ */
+class AlgoMigration_102 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('algo', [
+                'columns' => [
+                    new Column(
+                        'id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'unsigned' => true,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'size' => 11,
+                            'first' => true
+                        ]
+                    ),
+                    new Column(
+                        'name',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'size' => 64,
+                            'after' => 'id'
+                        ]
+                    ),
+                    new Column(
+                        'status',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'default' => "1",
+                            'size' => 2,
+                            'after' => 'name'
+                        ]
+                    )
+                ],
+                'indexes' => [
+                    new Index('PRIMARY', ['id'], 'PRIMARY')
+                ],
+                'options' => [
+                    'TABLE_TYPE' => 'BASE TABLE',
+                    'AUTO_INCREMENT' => '1',
+                    'ENGINE' => 'InnoDB',
+                    'TABLE_COLLATION' => 'utf8mb4_general_ci'
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
---
+++ app/migrations/1.0.2/wallet.php
@@ -0,0 +1,159 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class WalletMigration_102
+ */
+class WalletMigration_102 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('wallet', [
+                'columns' => [
+                    new Column(
+                        'id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'unsigned' => true,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'size' => 11,
+                            'first' => true
+                        ]
+                    ),
+                    new Column(
+                        'user_id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'size' => 11,
+                            'after' => 'id'
+                        ]
+                    ),
+                    new Column(
+                        'address',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'size' => 255,
+                            'after' => 'user_id'
+                        ]
+                    ),
+                    new Column(
+                        'public_key',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'size' => 255,
+                            'after' => 'address'
+                        ]
+                    ),
+                    new Column(
+                        'private_key',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'size' => 255,
+                            'after' => 'public_key'
+                        ]
+                    ),
+                    new Column(
+                        'name',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'size' => 64,
+                            'after' => 'private_key'
+                        ]
+                    ),
+                    new Column(
+                        'currency',
+                        [
+                            'type' => Column::TYPE_CHAR,
+                            'size' => 5,
+                            'after' => 'name'
+                        ]
+                    ),
+                    new Column(
+                        'balance',
+                        [
+                            'type' => Column::TYPE_BIGINTEGER,
+                            'default' => "0",
+                            'size' => 18,
+                            'after' => 'currency'
+                        ]
+                    ),
+                    new Column(
+                        'created_at',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'size' => 11,
+                            'after' => 'balance'
+                        ]
+                    ),
+                    new Column(
+                        'updated_at',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'size' => 11,
+                            'after' => 'created_at'
+                        ]
+                    ),
+                    new Column(
+                        'deleted_at',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'size' => 11,
+                            'after' => 'updated_at'
+                        ]
+                    ),
+                    new Column(
+                        'status',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'default' => "1",
+                            'size' => 2,
+                            'after' => 'deleted_at'
+                        ]
+                    )
+                ],
+                'indexes' => [
+                    new Index('PRIMARY', ['id'], 'PRIMARY')
+                ],
+                'options' => [
+                    'TABLE_TYPE' => 'BASE TABLE',
+                    'AUTO_INCREMENT' => '3',
+                    'ENGINE' => 'InnoDB',
+                    'TABLE_COLLATION' => 'utf8mb4_general_ci'
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Creating Service Wallets (for pools, miners, commission fees, and etc)
+        // @todo
+
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
